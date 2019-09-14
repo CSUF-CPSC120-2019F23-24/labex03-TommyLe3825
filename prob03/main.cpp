26d84da6b13ad42eb8ff5d706b9da3ab517acd35
tommyle <tommyle@cs104u-6g.acad.fullerton.edu>
2019-09-14 15:05:58 -0700

    Height Comments Fixed

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -4,11 +4,12 @@
 int main()
 {
   int height;
-  std::cout << "Please enter the person's height in inches: " ;
-  std::cin >> height;
 
-  int heightfeet = height / 12;
-  double inches = height % 12;
+  std::cout << "Please enter the person's height in inches: " ;
+  std::cin >> height; // the input height
+
+  int heightfeet = height / 12; //decimal portion is cut off for feet
+  double inches = height % 12; //using modulus to give us remainder
   std::cout << "That person is " << heightfeet << "'" << inches << "\"" << std::endl;
 
   return 0;
